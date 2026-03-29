d44414d8aba8d6f403576a504102fca728c4e33f
Edwin <elias986@yahoo.com>
2026-03-29 01:10:23 -0700

    Delete front directory

--- front/flatpickr.php
+++
@@ -1,6 +0,0 @@
-<?php
-define('GLPI_ROOT', dirname(dirname(dirname(dirname(__DIR__)))));
-header('Content-Type: application/javascript; charset=UTF-8');
-header('Cache-Control: public, max-age=31536000, immutable');
-readfile(dirname(__DIR__) . '/js/flatpickr.min.js');
-exit;
